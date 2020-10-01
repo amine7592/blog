5f6db2e2c681a26d3ef9db2b5622e234b9b89976
Amine Nokra <amine.nokra@gmail.com>
2020-10-01 10:57:14 +0200

    project structure

---
+++ app/controllers/index.js
@@ -0,0 +1,33 @@
+var posts = [];
+
+var postContainer;
+var modal;
+var addPostBtn;
+
+
+
+function getPosts(){
+    //
+
+}
+
+
+function addPost(){
+
+
+}
+
+function closeModal(){
+
+}
+
+
+function openModal(){
+
+
+}
+
+
+function resetModal(){
+    
+}
--- js/_main.js
+++ js/_main.js
@@ -1,3 +1,12 @@
+var posts = [];
+
+posts.push(new Post("Titolo 2","dasadssd addsadsda adsdsa dsa saasd",true,false))
+posts.push(new Post("titolo 3","ldasda dasodsaodsa adsodoasdsa",false,true))
+posts.push(new Post("Titolo 4","dsadasd dsaasdasdas",true,false))
+posts.push(new Post("titolo 5","dasads sdads aasd",true,true))
+posts.push(new Post("Titolone","dasa dssd adsaasd  dsadsasddassadasd dasjkasd",true,false))
+
+console.log(posts);
 
 $(document).ready(function () {
 
@@ -16,6 +25,15 @@ $(document).ready(function () {
 
     });
 
+    for(var i = 0; i< posts.length; i++){
+        var post = posts[i];
+        if(post.public === true){
+            createUIPost(post);
+        }
+
+
+    }
+
     
 
     $("#savePostBtn").click(addPost);
@@ -24,14 +42,51 @@ $(document).ready(function () {
 });
 
 
+function createUIPost(post){
+    var postContainer = $("#postContainer").clone();
+    postContainer.css("display","block");
+    postContainer.attr("id","");
+    postContainer.addClass("class","postContainer");
+
+    var postHeader = postContainer.find(".card-header");
+    var postBody = postContainer.find(".card-text");
+
+    postHeader.html(post.title);
+    postBody.html(post.body);
+
+    $("#postsRow").append(postContainer);
+
+}
+
+
+function closeModal(){
+    $("#newPostModal").modal("hide");
+}
+
+function resetModal(){
+    $("#postTitle").val("");
+    $("#postBody").val("");
+
+}
+
 
 function addPost(){
+
+    closeModal();
 
     //creare un item HTML e aggiungerlo nel DOM.
     var title = $("#postTitle").val();
     var body = $("#postBody").val();
+    var public = $("publicCheck").val();
+    var featured = $("featuredCheck").val();
 
-    var post = new Post(title,body,true);
+    resetModal();
+
+    var post = new Post(title,body,public,featured);
+    createUIPost(post);
+
+
+   
 
 
 
