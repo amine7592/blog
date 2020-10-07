a876ab8abdbf5bf4fb9d9ee58f6b3de30ef78857
Amine Nokra <amine.nokra@gmail.com>
2020-10-07 09:31:36 +0200

    api integration

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,131 @@
-var posts = [];
+var postController = function () {
 
-var postContainer;
-var modal;
-var addPostBtn;
+    this. posts = [];
+
+    this.postsContainer;
+    this.postContainer;
+    this.modal;
+    this.openModalBtn;
+    this.modalTitle;
+    this.modalDescription;
+    this.modalPublicCheck;
+    this.addPostBtn;
+
+    function init() {
+
+        
+
+        $(document).ready(function () {
+            postsRow = $("#postsRow");
+            postContainer = $("#postContainer");
+            modal = $("#newPostModal");
+            modalTitle = $("#postTitle");
+            modalBody = $("#postDescription");
+            modalCheck = $("#publicCheck");
+            addPostBtn = $("#savePostBtn");
+            var posts;
+
+            $.get({
+                url: "https://api.npoint.io/24620ef625c768a4f3c4",
+                dataType: "text",
+                success: function(data,textStatus,jqXHR){
+                        console.log("data",JSON.parse(data));
+                        posts = JSON.parse(data);
+
+                        console.log(JSON.stringify(posts));
+
+
+                        for(var i = 0; i< posts.length; i++){
+                            var post = posts[i];
+                            if(post.public === true){
+                                createUIPost(post);
+                            }
+                        }
 
 
 
-function getPosts(){
-    //
-
-}
+                }
+              });
 
 
-function addPost(){
+            addPostBtn.click(function(){
+                var post = new Post("adsdsa","asddasdas",true,false);
+                addPost(post);
+            });
+
+           
 
 
-}
+        });
 
-function closeModal(){
-
-}
+    }
 
 
-function openModal(){
+    function getPosts() {
+        let posts = []
+        posts.push(new Post("Titolo 2", "dasadssd addsadsda adsdsa dsa saasd", true, false))
+        posts.push(new Post("titolo 3", "ldasda dasodsaodsa adsodoasdsa", false, true))
+        posts.push(new Post("Titolo 4", "dsadasd dsaasdasdas", true, false))
+        posts.push(new Post("titolo 5", "dasads sdads aasd", true, true))
+        posts.push(new Post("Titolone", "dasa dssd adsaasd  dsadsasddassadasd dasjkasd", true, false))
+        return posts;
+    }
 
 
-}
+    function addPost(post) {
 
-
-function resetModal(){
+        console.log("post",post);
+        var postContainer = $("#postContainer").clone();
+        postContainer.css("display","block");
+        postContainer.attr("id","");
+        postContainer.addClass("class","postContainer");
     
+        var postHeader = postContainer.find(".card-header");
+        var postBody = postContainer.find(".card-text");
+    
+        postHeader.html(post.title);
+        postBody.html(post.body);
+    
+        $("#postsRow").append(postContainer);
+
+
+
+    }
+
+    function createUIPost(post){
+        var postContainer = $("#postContainer").clone();
+        postContainer.css("display","block");
+        postContainer.attr("id","");
+        postContainer.addClass("class","postContainer");
+    
+        var postHeader = postContainer.find(".card-header");
+        var postBody = postContainer.find(".card-text");
+    
+        postHeader.html(post.title);
+        postBody.html(post.body);
+    
+        $("#postsRow").append(postContainer);
+    
+    }
+
+    function closeModal() {
+
+    }
+
+
+    function openModal() {
+
+
+    }
+
+
+    function resetModal() {
+
+    }
+
+    return {
+        init : init,
+        addPost:addPost,
+    };
+
 }
--- js/_main.js
+++ js/_main.js
@@ -6,89 +6,75 @@ posts.push(new Post("Titolo 4","dsadasd dsaasdasdas",true,false))
 posts.push(new Post("titolo 5","dasads sdads aasd",true,true))
 posts.push(new Post("Titolone","dasa dssd adsaasd  dsadsasddassadasd dasjkasd",true,false))
 
-console.log(posts);
 
-$(document).ready(function () {
-
-    var users = [
-        'mario', 'giulio', 'alberto', 'claudia'
-    ]
-
-    $(".send-comment").click(function () {
-        var commentText = $(".comment-text").val();
-
-        var commentContainer = $(".comments-container");
-        var commentRow = '<li class="list-group-item"><a href="#" class="badge badge-light">' + users[Math.round(Math.random(0, 3))
-        ] + '</a>' + commentText + '</li>';
-
-        commentContainer.append(commentRow);
-
-    });
-
-    for(var i = 0; i< posts.length; i++){
-        var post = posts[i];
-        if(post.public === true){
-            createUIPost(post);
-        }
+new postController().init();
 
 
-    }
+
+
+
+
+
+
+
+// $(document).ready(function () {
+
+
+//     // var users = [
+//     //     'mario', 'giulio', 'alberto', 'claudia'
+//     // ]
+
+//     // $(".send-comment").click(function () {
+//     //     var commentText = $(".comment-text").val();
+
+//     //     var commentContainer = $(".comments-container");
+//     //     var commentRow = '<li class="list-group-item"><a href="#" class="badge badge-light">' + users[Math.round(Math.random(0, 3))
+//     //     ] + '</a>' + commentText + '</li>';
+
+//     //     commentContainer.append(commentRow);
+
+//     // });
+
+    // for(var i = 0; i< posts.length; i++){
+    //     var post = posts[i];
+    //     if(post.public === true){
+    //         createUIPost(post);
+    //     }
+
+
+//     // }
 
     
 
-    $("#savePostBtn").click(addPost);
+//     // $("#savePostBtn").click(addPost);
 
 
-});
+// });
 
 
-function createUIPost(post){
-    var postContainer = $("#postContainer").clone();
-    postContainer.css("display","block");
-    postContainer.attr("id","");
-    postContainer.addClass("class","postContainer");
+// function createUIPost(post){
+//     var postContainer = $("#postContainer").clone();
+//     postContainer.css("display","block");
+//     postContainer.attr("id","");
+//     postContainer.addClass("class","postContainer");
 
-    var postHeader = postContainer.find(".card-header");
-    var postBody = postContainer.find(".card-text");
+//     var postHeader = postContainer.find(".card-header");
+//     var postBody = postContainer.find(".card-text");
 
-    postHeader.html(post.title);
-    postBody.html(post.body);
+//     postHeader.html(post.title);
+//     postBody.html(post.body);
 
-    $("#postsRow").append(postContainer);
+//     $("#postsRow").append(postContainer);
 
-}
-
-
-function closeModal(){
-    $("#newPostModal").modal("hide");
-}
-
-function resetModal(){
-    $("#postTitle").val("");
-    $("#postBody").val("");
-
-}
+// }
 
 
-function addPost(){
+// function closeModal(){
+//     $("#newPostModal").modal("hide");
+// }
 
-    closeModal();
+// function resetModal(){
+//     $("#postTitle").val("");
+//     $("#postBody").val("");
 
-    //creare un item HTML e aggiungerlo nel DOM.
-    var title = $("#postTitle").val();
-    var body = $("#postBody").val();
-    var public = $("publicCheck").val();
-    var featured = $("featuredCheck").val();
-
-    resetModal();
-
-    var post = new Post(title,body,public,featured);
-    createUIPost(post);
-
-
-   
-
-
-
-}
-
+// }
